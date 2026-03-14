1b7fba2804a664c7db840ce69512c63ab06e5037
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-14 20:49:24 +0800

    Update check.ts

--- api/check.ts
+++ api/check.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import originalServerHelpers from "../server/original_server_helpers.cjs";
+const originalServerHelpers: any = require("../server/original_server_helpers.cjs");
 
 const {
   getCookieHeaders,
