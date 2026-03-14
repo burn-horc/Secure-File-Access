8745f6070c1ff2137a229209d3ee1a872980a58b
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-15 00:42:18 +0800

    Create supabase.ts

---
+++ api/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url = process.env.SUPABASE_URL!;
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabase = createClient(url, key, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+  },
+});
