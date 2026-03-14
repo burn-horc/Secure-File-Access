86f0abaa2b681bfac6ff4a4571c15d45b971dec6
burn-horc <bernardhorcasitas24@gmail.com>
2026-03-14 22:17:53 +0800

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,8 +1,7 @@
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from "@shared/schema";
-import * as authSchema from "@shared/models/auth";
-
+import * as schema from "../shared/schema";
+import * as authSchema from "../shared/models/auth";
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
