ea9e4d6bd224adec461fb9377aaa9209d946d891
burn-horc <bernardhorcasitas24@gmail.com>
2026-08-02 11:01:24 +0800

    Update CheckerPage.jsx

--- client/src/CheckerPage.jsx
+++ client/src/CheckerPage.jsx
@@ -2337,7 +2337,7 @@ animation={isPremiumPage ? premiumAnimation : undefined}
         textTransform="uppercase"
         mb={1}
       >
-        Code delivery
+        NOTE
       </Text>
 
       <Text
